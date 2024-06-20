99ccfe9e3d18dd416e4e644d024c5da24c676860
AdrienHdz <adrien.hernandez.17@gmail.com>
2024-06-20 18:34:59 +0000

    feat: added sfr-embedding-mistral original model

--- test_gguf_models.py
+++ test_gguf_models.py
@@ -1,8 +1,16 @@
 from llama_cpp import Llama
+from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer, util
+
+model_original = SentenceTransformer("models/sfr-embedding-mistral")
 model = Llama("models/sfr-embedding-mistral.f16.gguf", embedding=True)
 modeltrue = Llama("ggml-sfr-embedding-mistral-f16.gguf", embedding=True)
 
+embed_original = model_original.encode(["hello world"])
 embed = model.embed("hello world")
-embedtrue = model.embed("hello world")
+embedtrue = modeltrue.embed("hello world")
 
-print(embed == embedtrue)
+print(embed_original.shape)
+print(embed.shape)
+print(embedtrue.shape)
+
